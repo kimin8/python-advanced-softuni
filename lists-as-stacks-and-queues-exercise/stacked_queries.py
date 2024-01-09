c674713d0e654dce2e2a1bae8abb8e38e7bc4318
Kimi Nikolov <126898516+kimin8@users.noreply.github.com>
2024-01-10 01:34:10 +0200

    Lists As Stacks and Queues [unfinished]

---
+++ lists-as-stacks-and-queues-exercise/stacked_queries.py
@@ -0,0 +1,18 @@
+n = int(input())
+stack = []
+
+for i in range(n):
+    command = input()
+
+    if "1" in command:
+        _, number = command.split(" ")
+        stack.append(int(number))
+    elif command == "2":
+        if len(stack) > 0:
+            stack.pop()
+    elif command == "3":
+        print(max(stack))
+    elif command == "4":
+        print(min(stack))
+
+print(", ".join([str(x) for x in stack][::-1]))
