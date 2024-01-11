051c529454ba790dc84f43c82c1a3ff5d5174e15
kiminikolov <bytebuilder220@gmail.com>
2024-01-11 21:46:13 +0200

    Added new files

--- lists-as-stacks-and-queues-exercise/truck_tour.py
+++ lists-as-stacks-and-queues-exercise/truck_tour.py
@@ -1,21 +1,22 @@
 from collections import deque
 
-n = int(input())
-road = deque()
-current_petrol = 0
-possible = []
+pumps_data = deque([[int(x) for x in input().split()] for _ in range(int(input()))])
 
-for i in range(n):
-    amount_of_petrol, distance_to_next = input().split(" ")
-    amount_of_petrol = int(amount_of_petrol)
-    distance_to_next = int(distance_to_next)
-    road.append([amount_of_petrol, distance_to_next])
+pumps_data_copy = pumps_data.copy()
+gas_in_tank = 0
+index = 0
 
-for i in range(len(road)):
-    if road[0][0] + current_petrol < road[0][1]:
-        road.rotate(-1)
+while pumps_data_copy:
+    petrol, distance = pumps_data_copy.popleft()
+
+    gas_in_tank += petrol
+
+    if gas_in_tank >= distance:
+        gas_in_tank -= distance
     else:
-        current_petrol += road[0][0] - road[0][1]
-        possible.append(i)
+        pumps_data.rotate(-1)
+        pumps_data_copy = pumps_data.copy()
+        index += 1
+        gas_in_tank = 0
 
-print(min(possible))
+print(index)
